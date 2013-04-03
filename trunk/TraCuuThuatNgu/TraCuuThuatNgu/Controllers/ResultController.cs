8c0a21546f4d7bedc2254669f2c41873ddc2bb41
khanhsnguyen@gmail.com <khanhsnguyen@gmail.com@43e39f9d-0b60-6fef-d670-4d5014dac656>
2013-04-03 10:08:55 +0000

    3-4-2013

--- trunk/TraCuuThuatNgu/TraCuuThuatNgu/Controllers/ResultController.cs
+++ trunk/TraCuuThuatNgu/TraCuuThuatNgu/Controllers/ResultController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TraCuuThuatNgu.Models;
 
 namespace TraCuuThuatNgu.Controllers
 {
@@ -10,12 +11,13 @@ namespace TraCuuThuatNgu.Controllers
     {
         //
         // GET: /Result/
+        TraCuuThuatNguEntities entity = new TraCuuThuatNguEntities();
 
         public ActionResult Index(string keyword)
         {
+            ThuatNgu thuatngu = entity.ThuatNgus.Find(keyword);
             ViewBag.Keyword = keyword;
             return View();
         }
-
     }
 }
