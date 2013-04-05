0e3ad1f093d0f7a739030da5a951241b795d58b6
khanhsnguyen@gmail.com <khanhsnguyen@gmail.com@43e39f9d-0b60-6fef-d670-4d5014dac656>
2013-04-05 10:18:16 +0000

    5-4-2013

---
+++ TraCuuThuatNgu/TraCuuThuatNgu/Controllers/LikeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TraCuuThuatNgu.Models;
+
+namespace TraCuuThuatNgu.Controllers
+{
+    public class LikeController : Controller
+    {
+        //model, work with database
+        LikeModel likeModel = new LikeModel();
+
+        //
+        // GET: /Like/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Like/Add
+        [HttpPost]
+        public ActionResult Add(string headWord)
+        {
+            int result = likeModel.Add(headWord);
+
+            if (result == LikeModel.SUCCESS)
+            {
+                return Json(new { message = "SUCCESS" }); 
+            }
+            else if (result == LikeModel.EXISTED)
+            {
+                return Json(new { message = "EXISTED" }); 
+            }
+            else {
+                return Json(new { message = "FAIL" }); 
+            }
+        }
+
+    }
+}
---
+++ TraCuuThuatNgu/TraCuuThuatNgu/Models/SearchHistoryModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TraCuuThuatNgu.Models
+{
+    public class SearchHistoryModel
+    {
+        TraCuuThuatNguEntities context = null;
+
+        public SearchHistoryModel()
+        {
+            context = new TraCuuThuatNguEntities();
+        }
+
+        //It has not done yet!
+        //add searching history 
+        public int AddHistory(string keyword)
+        {
+            try
+            {
+                SearchHistory searchHistory = context.SearchHistories.Find(keyword);
+                if (searchHistory == null)
+                {
+                    searchHistory = new SearchHistory();
+                    searchHistory.Keyword = keyword;
+                    searchHistory.IsExist = true;
+                    searchHistory.Counter = 0;
+                    context.SearchHistories.Add(searchHistory);
+                }
+                else
+                {
+                    searchHistory.IsExist = true;
+                    searchHistory.Counter++;
+
+                    //context.SearchHistories.
+                }
+
+            }
+            catch
+            { }
+
+        }
+    }
+}
