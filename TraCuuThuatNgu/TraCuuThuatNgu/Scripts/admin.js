bf1c0e3b64d4b3524532f3c66c9ecc53d6b1e62f
khanhsnguyen@gmail.com <khanhsnguyen@gmail.com@43e39f9d-0b60-6fef-d670-4d5014dac656>
2013-04-24 10:28:47 +0000

    24-4

--- TraCuuThuatNgu/TraCuuThuatNgu/Scripts/admin.js
+++ TraCuuThuatNgu/TraCuuThuatNgu/Scripts/admin.js
@@ -6,17 +6,17 @@ $(document).ready(function () {
     $("#order-search-history").change(function () {
 
         var url = window.location.href;
-        var indexOfOrderBy = url.indexOf("&orderby");
-        var indexOfPage = url.indexOf("page");
+        var indexOfOrderBy = url.indexOf("orderby");
+        var indexOfPage = url.indexOf("?");
 
-        if (indexOfOrderBy == -1) {
-            if (indexOfPage == -1) {
-                url = window.location + "?orderby=" + $("#order-search-history").val();
+        if (indexOfPage != -1) {
+            if (indexOfOrderBy != -1) {
+                url = url.substr(0, indexOfOrderBy) + "orderby=" + $("#order-search-history").val();                
             } else {
                 url = window.location + "&orderby=" + $("#order-search-history").val();
             }
         } else {
-            url = url.substr(0, indexOfOrderBy) + "&orderby=" + $("#order-search-history").val(); ;
+            url = window.location + "?orderby=" + $("#order-search-history").val();           
         }
         window.location.href = url;
 
